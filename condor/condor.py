dea3010330f0d191975ea53b5cd36578b87fd7d3
Genís Calderer <33183260+genisott@users.noreply.github.com>
2019-08-27 16:12:57 +0200

    Rebuilt the whole program

--- condor/condor.py
+++ condor/condor.py
@@ -1,24 +1,40 @@
 import numpy as np
 import pandas as pd
-import time
 from igraph import *
+import time
 
-def matrices(net,genes,regs):
-    """Builds the adjacency matrix and the modularity matrix."""
-    # Building matrix A
-    ls = {(genes[net.iloc[i,0]],regs[net.iloc[i,1]]):net.iloc[i,2] for i in range(0,len(net))}
-    A = np.matrix(np.zeros((len(genes),len(regs))))
-    for i in ls:
-        A[i]=ls[i]
-      
-    ki = A.sum(1)
-    dj = A.sum(0)   
-    m = float(sum(ki))
-    B = A - ((ki@dj)/m)
+def condor_object(net):
+    """Initialization of the condor object. The function gets a network in edgelist format encoded in a pandas dataframe.
+    Returns a dictionary with an igraph network, names of the targets and regulators, list of edges, modularity, and vertex memberships.    
+    """
     
+    t = time.time()
+        #Error flags.
+    assert len(set(net.iloc[:,0]).intersection(net.iloc[:,1]))==0, "The network must be bipartite."
+    assert not net.isnull().any().any(), "NaN values detected."
+    assert not ("" in list(net.iloc[:,0]) or "" in list(net.iloc[:,1])), "Empty strings detected."
     
-    return A,B,m
-
+    #Builds graph object.
+    if (net.shape[1] == 3): 
+        print("Weights detected")
+        edges = list(zip(net.iloc[:,0],net.iloc[:,1],net.iloc[:,2]))
+        Gr = Graph.TupleList(edges,weights=True)
+    else:
+        print("Unweighted network. Weights initialized as 1.")
+        edges = list(zip(net.iloc[:,0],net.iloc[:,1],[1 for i in net.iloc[:,1]]))
+        Gr = Graph.TupleList(edges,weights=True)
+    
+    #Assigns color names (bipartite sets).
+    reg_names = sorted(set(net.iloc[:,1]))
+    tar_names = sorted(set(net.iloc[:,0]))
+    Gr.vs["type"] = 0 #Tar
+    for j in [i.index for i in Gr.vs if i["name"] in reg_names]:
+        Gr.vs[j]["type"] = 1 #Reg
+    
+    index_dict = {k.index:k["name"] for k in Gr.vs  }
+    
+    print("Condor object built in",time.time()-t)
+    return {"G":Gr,"tar_names":tar_names,"reg_names":reg_names,"index_dict":index_dict,"edges":edges,"modularity":None,"reg_memb":None,"tar_memb":None}
 
 def bipartite_modularity(B,m,R,T):  
     """Computation of the bipartite modularity as described in ""Modularity and community detection in bipartite networks" by Michael J. Barber." """
@@ -26,10 +42,51 @@ def bipartite_modularity(B,m,R,T):
     Q = (1/m)*(np.trace(RtBT))
     return Q
 
+def initial_community(CO,method="LCS",project=False):
+    """Computation of the initial community structure based on unipartite methods.
+    The implementation using bipartite projection is not yet available, but project=False performs better modularity-wise (at least with the networks I worked on).
+    """
+    
+    
+    if project: print("Not yet implemented.")
+    
+    t = time.time()
+    
+    #Computes initial community assignement based on different methods. Default method is Louvain clustering which is really fast. The others take several times more.
+    if not project:
+        if method=="LCS":
+            vc = Graph.community_multilevel(CO["G"],weights="weight")
+        if method=="LEC":
+            vc = Graph.community_leading_eigenvector(CO["G"],weights="weight")
+        if method=="FG":
+            vc = Graph.community_fastgreedy(CO["G"],weights="weight").as_clustering()
+    print("Initial community structure computed in ",time.time()-t,". Modularity = ",vc.modularity)
+     
+    CO["modularity"] = vc.modularity  
+    
+    #Stores initial community in the condor object.
+    reg_index = [i.index for i in CO["G"].vs.select(type_in=[1])]
+    reg_memb = [vc.membership[i] for i in reg_index]
+    T0 = pd.DataFrame(zip(reg_index,reg_memb))
+    T0.columns = ["index","community"]    
+    CO["reg_memb"] = T0
+    
+        
+    return CO 
 
+def brim(CO,deltaQmin="def",c=25):
+    """Implementation of the BRIM algorithm to iteratively maximize bipartite modularity.
+    Note that c is the maximum number of communities. Dynamic choice of c is not yet implemented.
+    """    
 
-def max_modularity(B,m,R0,T0,c,deltaQmin):
-    """Implementation of the BRIM algorithm to iteratively maximize bipartite modularity."""
+    #Gets modularity matrix, initial community matrix and index dictionary.
+    B,m,T0,gn,rg = matrices(CO,c)
+    
+    #Default deltaQmin.
+    if(deltaQmin == "def"):
+        deltaQmin = min(1/len(CO["edges"]),1e-5)
+    
+    #BRIM iterative process
     Qnow = 0
     deltaQ = 1
     p,q = B.shape
@@ -52,88 +109,64 @@ def max_modularity(B,m,R0,T0,c,deltaQmin):
         Qnow = bipartite_modularity(B,m,R,T)
         deltaQ = Qnow-Qthen
         print(Qnow)
-    return R,T    
+        
+    #Update modularity attribute   
+    CO["modularity"] = Qnow
+    
+    #Update membership dataframes.   
+    CO["tar_memb"] = pd.DataFrame(list(zip(list(gn),[R[i,:].argmax() for i in range(0,len(gn))])))
+    CO["reg_memb"] = pd.DataFrame(list(zip(list(gn),[T[i,:].argmax() for i in range(0,len(rg))]))) 
+    CO["tar_memb"].columns = ["tar","com"]
+    CO["reg_memb"].columns = ["tar","com"]
+        
+        
+    return CO
 
-
-
-def initial_communities(A,gn,rg,c):
-    """Uses multilevel community detection from the package python-igraph to create the initial community structure."""
+def matrices(CO,c):
+    """Computation of modularity matrix and initial community matrix."""
     
-    p,q = len(gn),len(rg)
+    t = time.time()
     
-    Am = np.zeros((p+q,p+q))
-    Am[:p, p:] = A
-    Am[p:,:p] = A.transpose()
+    #Dimensions of the matrix
+    p = len(CO["tar_names"])
+    q = len(CO["reg_names"])
     
-    grn = Graph.Weighted_Adjacency(Am.tolist(), mode=ADJ_UNDIRECTED, attr="weight")
-    n = p+q
-    vc = Graph.community_multilevel(grn,weights="weight")
+    #Index dictionaries for the matrix. Note that this set of indices is different of that in the CO object (that one is for the igraph network.)
+    rg = {CO["reg_names"][i]: i for i in range(0,q)}
+    gn = {CO["tar_names"][i]: i for i in range(0,p)}
     
+    #Computes bipartite adjacency matrix.
+    A = np.matrix(np.zeros((p,q)))
+    for edge in CO["edges"]:
+        A[gn[edge[0]],rg[edge[1]]] = edge[2]
     
-    M = np.zeros((n,c))
-    for i in range(0,len(vc)):
-        for j in vc[i]:
-            M[j,i]=1
-    R,T = M[:len(gn),],M[len(gn):,]
+    #Computes bipartite modularity matrix.
+    ki = A.sum(1)
+    dj = A.sum(0)   
+    m = float(sum(ki))
+    B = A - ((ki@dj)/m)
     
-    return R,T
-
-
+    #Creates initial community T0 matrix.
+    d = CO["index_dict"]
+    ed = zip([rg[j] for j in [d[i] for i in CO["reg_memb"]["index"]]],CO["reg_memb"]["community"])
+    T0 = np.zeros((q,c))
+    for edge in ed:
+        T0[edge]=1
+    
+    print("Matrices computed in",time.time()-t)
+    return B,m,T0,gn,rg
 
 def condor(filename,c=25,deltaQmin="def"):
-    """Main function of the pyCONDOR package.
-    This function reads a filename which should be a tab-separated file containing edges and weights for a bipartite graph.
-    c is the max number of communities that can be found.
-    deltaQmin is the tolerance for the iterative maximization of the modularity.
+    """Default settings run of condor with output of the membership dataframes.
+    Reads a network in csv format and index_col=0."""
     
-    IMPORTANT: The network MUST be bipartite and connected, otherwise the results are meaningless.
     
-    The function outputs two files with the communities to which the vertices of each of the bipartite sets belong. It also 
-    returns the matrices Rf,Tf of membership to communities.    
+    net = pd.read_csv(filename,sep=",",index_col=0)
+    CO = condor_object(net)
+    CO = initial_community(CO)
+    CO = brim(CO,deltaQmin,c)
     
-    This method uses the algorithm descibed in "Modularity and community detection in bipartite networks" by Michael J. Barber.
-    and is somewhat translated from the R package CONDOR https://github.com/jplatig/condor described in the paper
-    "Bipartite Community Structure of eQTLs" by John Platig , Peter J. Castaldi, Dawn DeMeo, John Quackenbush. 
+    CO["tar_memb"].to_csv("tar_memb.txt")
+    CO["reg_memb"].to_csv("reg_memb.txt")
     
-    This python version is a bit more limited than the R package in terms of options.
-    
-    """
-    tt = time.time()
-    #Loads the network file.
-    t = time.time()
-    net = pd.read_csv(filename,sep="\t",index_col=0)
-    
-    if(deltaQmin=="def"):
-        deltaQmin = min(1/len(net),1e-4)
-    
-    gn = list(sorted(set(net.iloc[:,0])))
-    genes = {gn[i]:i for i in range(0,len(gn))}
-    rg = list(sorted(set(net.iloc[:,1])))
-    regs = {rg[i]:i for i in range(0,len(rg))}
-    print("Data loaded.",time.time()-t)
-    
-    #Builds matrices A (tilde), B (tilde):
-    t = time.time()
-    A,B,m = matrices(net,genes,regs)
-    print("Matrices computed.",time.time()-t)
-    
-    #Computes initial community T0:
-    t = time.time()
-    R0,T0 = initial_communities(A,gn,rg,c)
-    print("Initial communities computed.",time.time()-t)
-    
-    #Maximize modularity:
-    t=time.time()
-    Rf,Tf = max_modularity(B,m,R0,T0,c,deltaQmin)
-    print("Modularity maximized.",time.time()-t)
-    
-    #Output to file:
-    t = time.time()
-    _,j = np.where(Rf>0)
-    pd.DataFrame({"gene":gn,"comm":list(j)}).to_csv("com_gene_"+filename+".txt")
-    _,j = np.where(Tf>0)
-    pd.DataFrame({"reg":rg,"comm":list(j)}).to_csv("com_reg_"+filename+".txt")
-    print("Communities exported.",time.time()-t)
-    
-    print("Total time:",time.time()-tt)
-    return Rf,Tf
+    return 0
